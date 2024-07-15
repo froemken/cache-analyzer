08368c3fea43dfc0f69cf309b9eca97291a58841
Stefan Froemken <froemken@gmail.com>
2024-07-15 03:45:01 +0200

    Add missing comma to CacheExpressionRepository

--- Classes/Domain/Repository/CacheExpressionRepository.php
+++ Classes/Domain/Repository/CacheExpressionRepository.php
@@ -55,7 +55,7 @@ class CacheExpressionRepository
                     'Error while querying table %s: %s',
                     self::TABLE,
                     $exception->getMessage(),
-                )
+                ),
             );
         }
 
